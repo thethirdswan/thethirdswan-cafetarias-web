8acd2c628d76bd2f4bb986dfceaa84d096d37223
TheThirdSwan <aryaswan17@gmail.com>
2024-12-27 18:42:10 +0800

    partial validation in login and signin

--- src/app/daftar/page.tsx
+++ src/app/daftar/page.tsx
@@ -1,17 +1,37 @@
+"use client";
+
 import { Button } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import signup from '../lib/signup';
+import { useState } from 'react';
 
 export default function Signup() {
+    const [username, setUsername] = useState("")
+    const [name, setName] = useState("")
+    function signupValid(e: any) {
+        if (name.length < 2) {
+            alert("Nama tidak boleh kurang dari dua huruf.");
+            e.preventDefault();
+        }
+        if (username.length < 2) {
+            alert("username ga boleh kurang dari dua huruf mmk");
+            e.preventDefault();
+        }
+        if (username.includes(" ")) {
+            alert("username ga boleh isi spasi kntl")
+            e.preventDefault();
+        }
+    }
+
     return (
         <Card className="mx-2 md:mx-[24.707vw]">
             <CardContent>
-                <form method='POST' action={signup}>
+                <form method='POST' onSubmit={signupValid} action={signup}>
                     <label htmlFor='username'>Nama</label><br/>
-                    <input type='text' name='nama' className='border' required/><br/>
+                    <input type='text' name='nama' className='border' required onChange={(e) => setName(e.target.value)}/><br/>
                     <label htmlFor='username'>Username</label><br/>
-                    <input type='text' name='username' className='border' required/><br/>
+                    <input type='text' name='username' className='border' required onChange={(e) => setUsername(e.target.value)}/><br/>
                     <label htmlFor='password'>Password</label><br/>
                     <input type='password' name='password' className='border' required/><br/>
                     <Button variant='contained' className='my-2'>
--- src/app/masuk/page.tsx
+++ src/app/masuk/page.tsx
@@ -1,15 +1,30 @@
+"use client";
+
 import { Button } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import login from '../lib/login';
+import { useState } from 'react';
 
 export default function Login() {
+    const [username, setUsername] = useState("")
+    function loginValid(e: any) {
+        if (username.length < 2) {
+            alert("username ga boleh kurang dari dua huruf mmk");
+            e.preventDefault();
+        }
+        if (username.includes(" ")) {
+            alert("username ga boleh isi spasi kntl");
+            e.preventDefault();
+        }
+    }
+
     return (
         <Card className="mx-2 md:mx-[24.707vw]">
             <CardContent>
-                <form method='POST' action={login}>
+                <form method='POST' onSubmit={loginValid} action={login}>
                     <label htmlFor='username'>Username</label><br/>
-                    <input type='text' name='username' className='border' required/><br/>
+                    <input type='text' name='username' className='border' required onChange={(e) => setUsername(e.target.value)}/><br/>
                     <label htmlFor='password'>Password</label><br/>
                     <input type='password' name='password' className='border' required/><br/>
                     <Button variant='contained' className='my-2'>
