f67ff0913ee810f3345a69559787b76527c19ef9
TheThirdSwan <aryaswan17@gmail.com>
2025-02-11 16:25:35 +0800

    partial order implementation

--- src/app/pesan/page.tsx
+++ src/app/pesan/page.tsx
@@ -5,6 +5,7 @@ import FoodSlot from "../ui/pages/foodslot";
 import DrinkSlot from "../ui/pages/drinkslot";
 
 export default function Pesan() {
+    const order = {}
     return (
         <main>
             <h3 className="text-center text-xl">Daftar Menu</h3>
@@ -21,7 +22,7 @@ export default function Pesan() {
                 </div>
                 <div>
                     <p className="text-center mr-[24.707vw]">Minuman</p>
-                    <Card className="mr-[24.707vw] ml-2 h-[75vh] overflow-scroll">
+                    <Card className="mr-[24.707vw] ml-2 h-[75vh] !overflow-scroll">
                         <CardContent>
                             <Suspense fallback={<Loading/>}>
                                 <DrinkSlot/>
--- src/app/ui/counters.tsx
+++ src/app/ui/counters.tsx
@@ -8,9 +8,30 @@ export default function Counters(item : any) {
     const [maxCounter, setMaxCounter] = useState(false)
     return (
         <div className="flex flex-row">
-            <button className="p-2 bg-cyan-300" disabled={minCounter} onClick={() => { console.log(counter); if (counter == 1) setMinCounter(true); setCounter(counter - 1); setMaxCounter(false); }}>-</button>
+            <button className="p-2 bg-cyan-300" disabled={minCounter} onClick={() => mincounter(item.item)}>-</button>
                 <p className="p-2">{counter}</p>
-            <button className="p-2 bg-cyan-300" disabled={maxCounter} onClick={() => { console.log(counter); if (counter == item.item.stok - 1) setMaxCounter(true); setCounter(counter + 1); setMinCounter(false)}}>+</button>
+            <button className="p-2 bg-cyan-300" disabled={maxCounter} onClick={() => maxcounter(item.item)}>+</button>
         </div>
     )
+
+    function mincounter(item : any) {
+        setCounter(counter - 1); 
+        global.orderlist[item.nama] = counter
+        setMaxCounter(false);
+        console.log(counter); 
+        console.log(global.orderlist); 
+        if (counter == 1) {
+            setMinCounter(true);
+            delete global.orderlist[item.nama]
+        }
+    }
+    
+    function maxcounter(item : any) {
+        setCounter(counter + 1); 
+        global.orderlist[item.nama] = counter
+        setMinCounter(false);
+        console.log(counter); 
+        console.log(global.orderlist); 
+        if (counter == item.stok - 1) setMaxCounter(true); 
+    }
 }
